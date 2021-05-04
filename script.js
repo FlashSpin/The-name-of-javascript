5be941fb3a64d5ed3059f7cd4bfc182e92f40e20
Michael Carlton <mccarlton95@gmail.com>
2021-05-04 14:25:18 +0100

    finished the if else and plolished up the propmt resonsis

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-let ans = prompt("What is the real name of javascript")
+let ans = prompt("What is the real name of javascript");
+
+if (ans == "ECMAScript"){
+    alert("Wow, you are clever!");
+}else{
+    alert('What? You didn\'t know "ECMAScript"! ');
+}
